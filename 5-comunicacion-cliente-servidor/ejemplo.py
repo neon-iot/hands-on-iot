de4fb4f2781dcdc303d1ee4b6e7d48a91056a3fa
r_russo <rodrigo.e.russo@gmail.com>
2022-06-06 12:47:00 -0300

    limpieza

---
+++ 5-comunicacion-cliente-servidor/ejemplo.py
@@ -0,0 +1,41 @@
+from time import sleep_ms
+
+import network
+from umqtt.robust import MQTTClient
+
+wlan = network.WLAN(network.STA_IF)
+if not wlan.active():
+    wlan.active(True)
+
+if not wlan.isconnected():
+    wlan.connect("LPMS", "truchard")
+
+    print("Conectando...")
+    while not wlan.isconnected():
+        sleep_ms(1000)
+
+config = wlan.ifconfig()
+print(f"Conectado con ip {config[0]}")
+
+
+def callback(topic, msg):
+    topic = topic.decode()
+    msg = msg.decode()
+
+    if topic == "/servidor":
+        print(f"Llegó {msg} de {topic}")
+
+
+cliente = MQTTClient("nombre", "10.2.13.83", keepalive=30)
+print("Conectando a servidor MQTT...")
+cliente.set_callback(callback)
+cliente.connect(clean_session=False)
+cliente.subscribe("#")
+print("Conectado")
+
+while True:
+    cliente.publish("/alumnos/yo", "hola")
+    cliente.check_msg()
+    sleep_ms(500)
+
+cliente.disconnect()
---
+++ 5-comunicacion-cliente-servidor/ejercicio.py
@@ -0,0 +1,86 @@
+from time import sleep_ms
+
+import network
+from umqtt.robust import MQTTClient
+
+from machine import Pin, PWM, ADC
+from neopixel import NeoPixel
+
+wlan = network.WLAN(network.STA_IF)
+if not wlan.active():
+    wlan.active(True)
+
+if not wlan.isconnected():
+    wlan.connect("LPMS", "truchard")
+
+    print("Conectando...")
+    while not wlan.isconnected():
+        sleep_ms(1000)
+
+config = wlan.ifconfig()
+print(f"Conectado con ip {config[0]}")
+
+led_verde = Pin(15, Pin.OUT)
+led_amarillo = Pin(2, Pin.OUT)
+led_rojo = Pin(0, Pin.OUT)
+boton = Pin(33, Pin.IN, Pin.PULL_UP)
+servo = PWM(Pin(14), freq=50)
+adc = ADC(Pin(35), atten=ADC.ATTN_11DB)
+np = NeoPixel(Pin(27), 1)
+
+estado_boton = False
+ultimo_estado_boton = boton.value()
+
+min_val = 51
+max_val = 102
+escala = (max_val - min_val) / 180
+
+def callback(topic, msg):
+    topic = topic.decode()
+    msg = msg.decode()
+
+    if topic == "/alumnos/yo/led_verde":
+        led_verde.value(msg == "1")
+    elif topic == "/alumnos/yo/led_amarillo":
+        led_amarillo.value(msg == "1")
+    elif topic == "/alumnos/yo/led_rojo":
+        led_rojo.value(msg == "1")
+    elif topic == "/alumnos/yo/servo":
+        ang = int(msg)
+        if ang < 0:
+            ang = 0
+        elif ang > 180:
+            ang = 180
+        servo.duty(int(ang * escala + min_val))
+    elif topic == "/alumnos/yo/neopixel":
+        colores = msg.split(",")
+        if len(colores) != 3:
+            return
+        r = int(colores[0])
+        g = int(colores[1])
+        b = int(colores[2])
+        
+        np[0] = (b, r, g)
+        np.write()        
+
+
+cliente = MQTTClient("nombre", "10.2.13.83", keepalive=30)
+print("Conectando a servidor MQTT...")
+cliente.set_callback(callback)
+cliente.connect(clean_session=False)
+cliente.subscribe("#")
+print("Conectado")
+
+while True:
+    cliente.check_msg()
+    
+    estado_boton = not boton.value()
+    if estado_boton != ultimo_estado_boton:
+        if estado_boton:
+            cliente.publish("/servidor/yo", str(adc.read_uv()))
+    
+    ultimo_estado_boton = estado_boton
+    
+    sleep_ms(10)
+
+cliente.disconnect()
